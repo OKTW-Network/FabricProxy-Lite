18387cca6cd1202321557410007711dd370384ec
James58899 <james59988@gmail.com>
2022-06-08 12:09:15 +0800

    Remove unused mixin

--- src/main/java/one/oktw/mixin/hack/ServerLoginNetworkHandler_EarlySendPacket.java
+++ src/main/java/one/oktw/mixin/hack/ServerLoginNetworkHandler_EarlySendPacket.java
@@ -7,7 +7,6 @@ import net.fabricmc.fabric.impl.networking.server.ServerLoginNetworkAddon;
 import net.minecraft.network.packet.c2s.login.LoginHelloC2SPacket;
 import net.minecraft.server.network.ServerLoginNetworkHandler;
 import one.oktw.VelocityLib;
-import one.oktw.mixin.ServerLoginNetworkHandler_DelayHello;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -16,25 +15,15 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ServerLoginNetworkHandler.class)
-public class ServerLoginNetworkHandler_EarlySendPacket implements ServerLoginNetworkHandler_DelayHello {
+public class ServerLoginNetworkHandler_EarlySendPacket {
     @Shadow
     @Nullable GameProfile profile;
-    private LoginHelloC2SPacket helloPacket = null;
 
     @Inject(method = "onHello", at = @At(value = "HEAD"), cancellable = true)
     private void skipKeyPacket(LoginHelloC2SPacket packet, CallbackInfo ci) {
         if (profile != null && profile.isComplete()) return; // Already receive profile form velocity.
 
-        helloPacket = packet;
         ((ServerLoginNetworkAddon) ((NetworkHandlerExtensions) this).getAddon()).sendPacket(VelocityLib.PLAYER_INFO_CHANNEL, PacketByteBufs.empty());
         ci.cancel();
-    }
-
-    @Nullable
-    @Override
-    public LoginHelloC2SPacket delayedHelloPacket() {
-        LoginHelloC2SPacket packet = helloPacket;
-        helloPacket = null; // Only read once.
-        return packet;
     }
 }
