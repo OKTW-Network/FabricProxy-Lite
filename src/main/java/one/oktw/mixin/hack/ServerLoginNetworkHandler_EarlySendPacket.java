aba18372506db5a7f5a8b6d007d8040157b116b5
James58899 <james59988@gmail.com>
2022-07-17 12:53:41 +0800

    Try compatible Quilt fix #25, fix #28

--- src/main/java/one/oktw/mixin/hack/ServerLoginNetworkHandler_EarlySendPacket.java
+++ src/main/java/one/oktw/mixin/hack/ServerLoginNetworkHandler_EarlySendPacket.java
@@ -1,16 +1,19 @@
 package one.oktw.mixin.hack;
 
 import com.mojang.authlib.GameProfile;
-import net.fabricmc.fabric.impl.networking.NetworkHandlerExtensions;
-import net.fabricmc.fabric.impl.networking.server.ServerLoginNetworkAddon;
+import net.minecraft.network.ClientConnection;
 import net.minecraft.network.packet.c2s.login.LoginHelloC2SPacket;
+import net.minecraft.network.packet.s2c.login.LoginQueryRequestS2CPacket;
 import net.minecraft.server.network.ServerLoginNetworkHandler;
 import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.concurrent.ThreadLocalRandom;
 
 import static one.oktw.VelocityLib.PLAYER_INFO_CHANNEL;
 import static one.oktw.VelocityLib.PLAYER_INFO_PACKET;
@@ -20,11 +23,15 @@ public class ServerLoginNetworkHandler_EarlySendPacket {
     @Shadow
     @Nullable GameProfile profile;
 
+    @Shadow
+    @Final
+    public ClientConnection connection;
+
     @Inject(method = "onHello", at = @At(value = "HEAD"), cancellable = true)
     private void skipKeyPacket(LoginHelloC2SPacket packet, CallbackInfo ci) {
         if (profile != null && profile.isComplete()) return; // Already receive profile form velocity.
 
-        ((ServerLoginNetworkAddon) ((NetworkHandlerExtensions) this).getAddon()).sendPacket(PLAYER_INFO_CHANNEL, PLAYER_INFO_PACKET);
+        connection.send(new LoginQueryRequestS2CPacket(ThreadLocalRandom.current().nextInt(), PLAYER_INFO_CHANNEL, PLAYER_INFO_PACKET));
         ci.cancel();
     }
 }
